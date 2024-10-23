3349613247f7262dc66f79aa0691a70c6df99351
u7111608 <u7111608@anu.edu.au>
2024-10-24 03:27:18 +1100

    script to generate 1 million queries

---
+++ generate_random_queries.sh
@@ -0,0 +1,13 @@
+./bin/generate_test_queries 264346 1000000 10 experiments/NY.q.s experiments/NY.q.t
+./bin/generate_test_queries 321270 1000000 10 experiments/BAY.q.s experiments/BAY.q.t
+./bin/generate_test_queries 425666 1000000 10 experiments/COL.q.s experiments/COL.q.t
+./bin/generate_test_queries 1070376 1000000 10 experiments/FLA.q.s experiments/FLA.q.t
+./bin/generate_test_queries 1207945 1000000 10 experiments/NW.q.s experiments/NW.q.t
+./bin/generate_test_queries 1524453 1000000 10 experiments/NE.q.s experiments/NE.q.t
+./bin/generate_test_queries 1890815 1000000 10 experiments/CAL.q.s experiments/CAL.q.t
+./bin/generate_test_queries 2758119 1000000 10 experiments/LKA.q.s experiments/LKA.q.t
+./bin/generate_test_queries 3598623 1000000 10 experiments/E.q.s experiments/E.q.t
+./bin/generate_test_queries 6262104 1000000 10 experiments/W.q.s experiments/W.q.t
+./bin/generate_test_queries 14081816 1000000 10 experiments/CTR.q.s experiments/CTR.q.t
+./bin/generate_test_queries 23947347 1000000 10 experiments/USA.q.s experiments/USA.q.t
+./bin/generate_test_queries 18010173 1000000 10 experiments/EUR.q.s experiments/EUR.q.t
